6ac15a0071993a1bdd2c3fc5d093a5dbb56808aa
Davis Yang <61362257+DavisHYang@users.noreply.github.com>
2024-08-05 18:55:43 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const info = {
+  TITLE: 'App'
+}
+
+const config = {
+  mode: 'development',
+  devtool: 'source-map',
+
+  devServer: {
+    static: path.join(__dirname, '../dist'),
+    port: 8080,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+    proxy: {
+       '/api': {
+            target: 'http://localhost:8080',
+            router: () => 'http://localhost:8088',
+            logLevel: 'debug'
+       }
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            experimentalWatchApi: true,
+          },
+        },
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(gltf|png)$/i,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(glsl)$/i,
+        use: ['file-loader'],
+      },
+    ]
+  },
+
+  resolve: {
+    extensions: [
+      '.tsx', '.ts', '.js', '.jsx', '.json', '.gltf', '.png'
+    ],
+    alias: {
+      three: path.resolve('./node_modules/three')
+    },
+  },
+
+  entry: {
+    main: './src/index.tsx'
+  },
+
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/'
+  },
+
+  plugins: [
+    new ProgressBarPlugin({ width: 80 }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin(["public/**/*", "public/**/*.png"]),
+    new MiniCssExtractPlugin({ filename: '[contenthash].css' }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: info.TITLE,
+      chunks: ['main'],
+      template: './src/index.html',
+      templateParameters: { TITLE: info.TITLE }
+    }),
+  ],
+}
+
+module.exports = [ config ];
